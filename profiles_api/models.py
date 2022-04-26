4c41bbfb774f31a71c79cdc8e2d72048aef705b4
romildosr07 <romildosr7@gmail.com>
2022-04-26 19:19:42 -0300

    feat: model UserProfile

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -3,3 +3,26 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 
 
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    #object = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Retrive full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """Retrive shot name of user"""
+        return self.name
+
+    def __str__(self):
+        """Return string representation of all users"""
+        return self.email
