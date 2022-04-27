b333a1c43b9c41121b3f9a588d4a375f61be4e14
romildosr07 <romildosr7@gmail.com>
2022-04-27 10:21:54 -0300

    Enable django admin user profile

--- profiles_projects/settings.py
+++ profiles_projects/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
     'profiles_api',
 
 ]
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
